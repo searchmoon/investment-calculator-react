2b3b9bc9f5717ab95c88ce62cc7c290b65260c3a
Rumi <moonding901212@gmail.com>
2023-12-16 23:37:13 +0900

    1. 컴포넌트 분리

---
+++ src/components/TableList/Td.jsx
@@ -0,0 +1,13 @@
+const Td = () => {
+  return (
+    <tr>
+      <td>YEAR NUMBER</td>
+      <td>TOTAL SAVINGS END OF YEAR</td>
+      <td>INTEREST GAINED IN YEAR</td>
+      <td>TOTAL INTEREST GAINED</td>
+      <td>TOTAL INVESTED CAPITAL</td>
+    </tr>
+  );
+};
+
+export default Td;
---
+++ src/components/TableList/Th.jsx
@@ -0,0 +1,5 @@
+const Th = () => {
+  return <div>Th</div>;
+};
+
+export default Th;
